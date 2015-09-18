544c7b776084f60817d9617654af839415a43c79
Ronald Kinard <furyhunter600@gmail.com>
2015-09-18 00:26:49 -0500

    A more extensive example.

--- src/main.rs
+++ src/main.rs
@@ -29,21 +29,37 @@ struct MyApplication {
 impl Application for MyApplication {
     fn main_loop(&mut self, _: &mut Apt) {
         use ctru::gfx::{Screen, Side};
-        use ctru::services::gsp::FramebufferFormat;
 
-        self.gfx.set_framebuffer_format(Screen::Top, FramebufferFormat::Bgr8);
         let (fb, _, _) = self.gfx.get_framebuffer(Screen::Top, Side::Left);
+        let (rfb, _, _) = self.gfx.get_framebuffer(Screen::Top, Side::Right);
+        let (bfb, _, _) = self.gfx.get_framebuffer(Screen::Bottom, Side::Left);
+
+        let topdepth = self.gfx.get_framebuffer_format(Screen::Top).pixel_depth_bytes();
+        let botdepth = self.gfx.get_framebuffer_format(Screen::Bottom).pixel_depth_bytes();
 
         gsp::wait_for_event(gsp::Event::VBlank0);
-        for i in fb.iter_mut() {
-            *i = 127u8;
+        for i in fb.chunks_mut(topdepth) {
+            i[0] = 127u8;
+            i[1] = 127u8;
+            i[2] = 127u8;
+        }
+        for i in rfb.chunks_mut(topdepth) {
+            i[0] = 0u8;
+            i[1] = 127u8;
+            i[2] = 255u8;
+        }
+        for i in bfb.chunks_mut(botdepth) {
+            i[0] = 255u8;
+            i[1] = 127u8;
+            i[2] = 0u8;
+
         }
         self.gfx.flush_buffers();
         self.gfx.swap_buffers();
 
         self.hid.scan_input();
         if self.hid.key_down(PadKey::Start) {
-            self.running = true;
+            self.running = false;
         }
     }
 
@@ -51,6 +67,9 @@ impl Application for MyApplication {
 }
 
 fn main_3ds() -> () {
+    use ctru::gfx::Screen;
+    use ctru::services::gsp::FramebufferFormat;
+
     let srv = match Srv::new() {
         Ok(s) => s,
         _ => return
@@ -63,7 +82,12 @@ fn main_3ds() -> () {
         Ok(h) => h,
         _ => return
     };
-    let gfx = Gfx::default();
+    let mut gfx = Gfx::default();
+
+    gfx.set_framebuffer_format(Screen::Top, FramebufferFormat::Bgr8);
+    gfx.set_framebuffer_format(Screen::Bottom, FramebufferFormat::Bgr8);
+
+    gfx.set_3d_enabled(true);
 
     let mut myapp = MyApplication {
         hid: hid,
