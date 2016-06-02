8d6a8bc8fbf2d9797f341faae1096152e7037e79
Fenrir <fenrirwolf@gmail.com>
2016-06-01 23:25:26 -0700

    Updated for latest ctru-rs API

--- Makefile
+++ Makefile
@@ -13,10 +13,10 @@ ifeq ($(strip $(RUST_3DS_SYSROOT)),)
 	RUST_3DS_SYSROOT := sysroot
 endif
 
-RUST_FLAGS := RUSTFLAGS="--sysroot sysroot -Z no-landing-pads"
+RUST_FLAGS := RUSTFLAGS="--sysroot sysroot -C panic=abort -Z no-landing-pads"
 #CARGO_FLAGS := --verbose 
 
-.PHONY: all clean $(CRATE_NAME) dist sysroot target/3ds/release/$(CRATE_NAME).elf
+.PHONY: all clean $(CRATE_NAME) dist sysroot test target/3ds/release/$(CRATE_NAME).elf
 
 all: $(CRATE_NAME) 
 
@@ -38,6 +38,10 @@ dist: $(CRATE_NAME)
 	cp target/3ds/release/$(CRATE_NAME).smdh dist/$(CRATE_NAME)
 	cp $(PROG_ICON) dist/$(CRATE_NAME)/$(CRATE_NAME).png
 
+test: $(CRATE_NAME)
+	citra target/3ds/release/$(CRATE_NAME).elf
+	
+
 clean:
 	rm -rf target
 	rm -rf dist
@@ -55,7 +59,7 @@ sysroot/lib/rustlib/3ds.json/lib:
 
 sysroot/lib/rustlib/3ds.json/lib/%.rlib: sysroot/lib/rustlib/3ds.json/lib
 	@echo Building $*
-	@rustc --target 3ds.json -o sysroot/lib/rustlib/3ds.json/lib/$*.rlib --sysroot $(RUST_3DS_SYSROOT) $(RUST_SRC_PATH)/$*/lib.rs -Z no-landing-pads
+	@rustc --target 3ds.json -o sysroot/lib/rustlib/3ds.json/lib/$*.rlib --sysroot $(RUST_3DS_SYSROOT) $(RUST_SRC_PATH)/$*/lib.rs -C panic=abort -Z no-landing-pads
 
 sysroot/lib/rustlib/3ds.json/lib/liblibc.rlib: 
 	@echo Building liblibc
--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,44 @@
 #![feature(start)]
-#![feature(alloc)]
-#![feature(collections)]
 #![no_main]
 #![no_std]
 
+#[macro_use]
 extern crate ctru;
-extern crate alloc;
-#[macro_use] extern crate collections;
+use ctru::prelude::*;
 
-use ctru::{Srv, Gfx};
-use ctru::services::{Hid, Apt, Application};
+use ctru::Gfx;
+use ctru::console::Console;
+use ctru::services::{Hid, Apt};
 use ctru::services::hid::PadKey;
-use ctru::services::gsp;
 
 #[no_mangle]
-pub extern fn main(_: isize, _: *const *const u8) -> isize {
+pub extern "C" fn main(_: isize, _: *const *const u8) -> isize {
     main_3ds();
     0
 }
 
-struct MyApplication {
-    hid: Hid,
-    gfx: Gfx,
-    running: bool
-}
-
-impl Application for MyApplication {
-    fn main_loop(&mut self, _: &mut Apt) {
-        use ctru::gfx::{Screen, Side};
-
-        let (fb, _, _) = self.gfx.get_framebuffer(Screen::Top, Side::Left);
-        let (rfb, _, _) = self.gfx.get_framebuffer(Screen::Top, Side::Right);
-        let (bfb, _, _) = self.gfx.get_framebuffer(Screen::Bottom, Side::Left);
-
-        let topdepth = self.gfx.get_framebuffer_format(Screen::Top).pixel_depth_bytes();
-        let botdepth = self.gfx.get_framebuffer_format(Screen::Bottom).pixel_depth_bytes();
-
-        gsp::wait_for_event(gsp::Event::VBlank0);
-        for i in fb.chunks_mut(topdepth) {
-            i[0] = 127u8;
-            i[1] = 127u8;
-            i[2] = 127u8;
-        }
-        for i in rfb.chunks_mut(topdepth) {
-            i[0] = 0u8;
-            i[1] = 127u8;
-            i[2] = 255u8;
-        }
-        for i in bfb.chunks_mut(botdepth) {
-            i[0] = 255u8;
-            i[1] = 127u8;
-            i[2] = 0u8;
-
-        }
-        self.gfx.flush_buffers();
-        self.gfx.swap_buffers();
-
-        self.hid.scan_input();
-        if self.hid.key_down(PadKey::Start) {
-            self.running = false;
-        }
-    }
-
-    fn ready_to_quit(&self) -> bool { !self.running }
-}
-
 fn main_3ds() -> () {
     use ctru::gfx::Screen;
-    use ctru::services::gsp::FramebufferFormat;
+    use ctru::services::gspgpu::FramebufferFormat;
 
-    let srv = match Srv::new() {
-        Ok(s) => s,
-        _ => return
-    };
-    let mut apt = match Apt::new() {
-        Ok(a) => a,
-        _ => return
-    };
-    let hid = match Hid::new() {
-        Ok(h) => h,
-        _ => return
-    };
+    let mut apt = Apt::new();
+    let mut hid = Hid::new();
     let mut gfx = Gfx::default();
 
     gfx.set_framebuffer_format(Screen::Top, FramebufferFormat::Bgr8);
     gfx.set_framebuffer_format(Screen::Bottom, FramebufferFormat::Bgr8);
 
-    gfx.set_3d_enabled(true);
+    let mut console = Console::default();
 
-    let mut myapp = MyApplication {
-        hid: hid,
-        gfx: gfx,
-        running: true
-    };
+    console.println("Hello, this is a walrus!");
 
-    apt.main_loop(&mut myapp);
+    while apt.main_loop() {
+        gfx.flush_buffers();
+        gfx.swap_buffers();
 
-    drop(myapp);
-    drop(apt);
-    drop(srv);
+        hid.scan_input();
+        if hid.key_down(PadKey::Start) {
+            break;
+        }
+    }
 }
